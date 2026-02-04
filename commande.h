de8eadcb64edc2a9c2722713ed7f381b65b82b6e
aminekallel2108 <amine.kallel@etudiant-enit.utm.tn>
2026-02-04 22:21:07 +0100

    Update commande.h

--- commande.h
+++ commande.h
@@ -1,18 +1,14 @@
 #ifndef COMMANDE_H_INCLUDED
 #define COMMANDE_H_INCLUDED
-
 #include "structures.h"
 
-#define MAX_COMMANDES 200
-
-// Commande function prototypes
-void afficher_Commandes(Commande commandes[], int nb_commandes);
-int Afficher_commande_par_id(Commande commandes[], int nb_commandes, int id);
-void Ajouter_Commande(Commande commandes[], int *nb_commandes, int capacite,
-                      Client *client, Pokemon *pokemon, int quantite);
-void modifier_Commande(Commande commandes[], int *nb_commandes, int id_commande,
-                       Client *nouveau_client, Pokemon *nouveau_pokemon,
-                       int nouvelle_quantite, EtatCommande nouvel_etat);
-int supprimer_Commande(Commande commandes[], int *nb_commandes, int id_commande);
+void afficher_Commandes(liste_commande* liste_commandes);
+void ajouter_Commande(liste_commande** liste_commandes, Client* client,Pokemon* pokemon, int quantite);
+void modifier_Commande(liste_commande* liste_commandes);
+void supprimer_Commande(liste_commande** liste_commandes);
+Commande* trouver_commande_par_id(liste_commande* liste_commandes, int id);
+int compter_commandes(liste_commande* liste_commandes);
+int compter_commandes_par_etat(liste_commande* liste_commandes, EtatCommande etat);
+void liberer_liste_commandes(liste_commande** liste_commandes);
 
 #endif // COMMANDE_H_INCLUDED
