edcd947face6367b557347fb0eeb726feda7f105
aminekallel2108 <amine.kallel@etudiant-enit.utm.tn>
2026-02-04 22:20:14 +0100

    Update structures.c

--- structures.c
+++ structures.c
@@ -8,7 +8,7 @@ int prochain_id_commande = 1;
 
 const char* type_to_string(TypePokemon type) {
     switch (type) {
-        case ELECTRIQUE: return "Électrique";
+        case ELECTRIQUE: return "Ã‰lectrique";
         case FEU: return "Feu";
         case EAU: return "Eau";
         case PLANTE: return "Plante";
